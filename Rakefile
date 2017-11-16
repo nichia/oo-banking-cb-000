9f084963693946cc6ee8d29a7dd0251838c8b026
Ni Chia <nichia@gmail.com>
2017-11-16 21:27:56 +0000

    Done.

---
+++ Rakefile
@@ -0,0 +1,17 @@
+require_relative './lib/bank_account'
+require_relative './lib/transfer'
+
+require 'pry'
+require 'rake'
+
+puts "Welcome to Backing and Transfer"
+
+def reload!
+  load "./lib/bank_account.rb"
+  load "./lib/transfer.rb"
+end
+
+desc "A Console: rake console"
+task :console do
+  pry.start
+end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    #if valid?
+    #  execute_transaction(sender, receiver, amount)
+    #end
+  end
+
+  def valid?
+    (@sender.valid?) && (@receiver.valid?) ? true : false
+  end
+
+  def execute_transaction
+    if valid? && status == "pending"
+      if (@sender.balance < @amount)
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      else
+        @sender.withdrawal(@amount)
+        @receiver.deposit(@amount)
+        @status = "complete"
+      end
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.withdrawal(@amount)
+      @sender.deposit(@amount)
+      @status = "reversed"
+    end
+  end
 end
